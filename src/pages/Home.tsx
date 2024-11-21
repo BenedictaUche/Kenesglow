88a2e2cc96dcc23621d0ead6f91ae7493a82e5c3
BenedictaUche <benedictaonyebuchi.uche@gmail.com>
2024-11-21 11:36:54 +0100

    added framer animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,25 +12,87 @@ import Gallery from "@/components/Gallery";
 import Stats from "@/components/Stats";
 import QuickLinks from "@/components/QuickLinks";
 import Trending from "@/components/Trending";
+import { motion} from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+
+const componentVariants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 3,
+    y: 0,
+    transition: {
+      duration: 1.5,
+    },
+  },
+};
+
+
 
 const Home = () => {
   return (
     <>
-      <HeroSection />
-      <Intro />
-      <Brands />
-      <Mantra />
-      <MarqueOne />
-      <QuickLinks />
-      <Trending />
-      <Gallery />
-      <Stats />
-      <Consultation />
-      <Testimonials />
-      <Footer />
-      <Subfooter />
+      <AnimatedComponent>
+        <HeroSection />
+      </AnimatedComponent>
+      <AnimatedComponent>
+        <Intro />
+      </AnimatedComponent>
+      <AnimatedComponent>
+        <Brands />
+      </AnimatedComponent>
+      <AnimatedComponent>
+        <Mantra />
+      </AnimatedComponent>
+      <AnimatedComponent>
+        <MarqueOne />
+      </AnimatedComponent>
+      <AnimatedComponent>
+        <QuickLinks />
+      </AnimatedComponent>
+      <AnimatedComponent>
+        <Trending />
+      </AnimatedComponent>
+      <AnimatedComponent>
+        <Gallery />
+      </AnimatedComponent>
+      <AnimatedComponent>
+        <Stats />
+      </AnimatedComponent>
+      <AnimatedComponent>
+        <Consultation />
+      </AnimatedComponent>
+      <AnimatedComponent>
+        <Testimonials />
+      </AnimatedComponent>
+      <AnimatedComponent>
+        <Footer />
+      </AnimatedComponent>
+      <AnimatedComponent>
+        <Subfooter />
+      </AnimatedComponent>
     </>
   );
 };
 
 export default Home;
+
+function AnimatedComponent({ children }: any) {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
+
+  return (
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      animate={inView ? 'visible' : 'hidden'}
+      variants={componentVariants}
+    >
+      {children}
+    </motion.div>
+  );
+}
