b1ab9182901c629d6b201475caa043a52d07cfce
BenedictaUche <benedictaonyebuchi.uche@gmail.com>
2024-11-21 03:34:29 +0100

    update: fixed mantra text

--- src/components/Mantra.tsx
+++ src/components/Mantra.tsx
@@ -4,16 +4,38 @@ import keneMirror from '../assets/images/brands/kene-mirror.mp4'
 
 const Mantra = () => {
   return (
-    <div className='opacity-90 relative'>
-      <video src={keneMirror} loop  autoPlay playsInline muted className='h-screen w-full' style={{inset: '-488.75px auto auto', objectFit: 'cover'}}>
+    <div className="opacity-90 relative">
+      {/* Background Video */}
+      <video
+        src={keneMirror}
+        loop
+        autoPlay
+        playsInline
+        muted
+        className="h-screen w-full object-cover"
+        style={{ inset: '-488.75px auto auto' }}
+      ></video>
 
-      </video>
-      <div className='left-[548px] top-[125px]  w-[568px] h-[313px] absolute' style={{transform: 'scale(1, 1) translate(0px, 0px) rotate(355deg)'}}>
-      <p className="text-[50px] text-white text-yellowtail bg-transparent h-full left-0 top-0 w-full">I believe that style can't be possessed, only discovered.<br />&nbsp;I love the process of helping women see the beauty they already possess.<br /></p>
+      {/* Text Overlay */}
+      <div
+        className="absolute"
+        style={{
+          left: '50%',
+          top: '20%',
+          width: '70%',
+          transform: 'translate(-50%, 0) rotate(355deg)',
+        }}
+      >
+        <p className="text-[6vw] md:text-[5vw] lg:text-[6vw] text-white text-yellowtail bg-transparent text-center leading-relaxed">
+          I believe that style can't be possessed, only discovered.
+
+          &nbsp;I love the process of helping women see the beauty they already
+          possess.
+          <br />
+        </p>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
 export default Mantra
