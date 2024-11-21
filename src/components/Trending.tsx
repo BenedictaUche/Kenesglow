d8f1b165f2f02c41e727cd1896218b530ff29c5f
BenedictaUche <benedictaonyebuchi.uche@gmail.com>
2024-11-21 09:57:15 +0100

    fix: changed link

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -1,11 +1,13 @@
 import viral from '../assets/images/viral.png';
+import mercy from '../assets/images/mercy.png';
+import cut from '../assets/images/cut-people.png';
 
 const Trending = () => {
   const items = [
-    { number: '01', title: 'This Video Went Really Viral On TikTok', action: 'WATCH THE VIDEO' },
-    { number: '02', title: 'My Episode on Goal Digger Podcast', action: 'LISTEN TO THE POD' },
-    { number: '03', title: 'The Hair Trick You 100% Need In Your Life', action: 'READ THE POST' },
-    { number: '04', title: 'My Scent Collab with Byredo Went Live!', action: 'CHECK IT OUT' },
+    { number: '01', title: 'This Video Went Really Viral On TikTok', action: 'WATCH THE VIDEO', image: viral, link: 'https://www.tiktok.com/@kenesgloww/video/7367787794283973921?is_from_webapp=1&sender_device=pc&web_id=7424044708274439685' },
+    { number: '02', title: 'My Episode With Mercy Eke On Love Is A Game Series', action: 'WATCH THE VIDEO', image: mercy, link: 'https://youtu.be/p7AeXmhPYNU?si=y0e40IORSnrUa8DV' },
+    { number: '03', title: 'My Take On Calling People Out', action: 'WATCH THE VIDEO', image: cut, link: 'https://www.tiktok.com/@kenesgloww/video/7433871125668482337?is_from_webapp=1&sender_device=pc&web_id=7424044708274439685' },
+    { number: '04', title: 'African Stars who were at the MET GALA - ANIKELA', action: 'READ THE POST', image: viral, link: 'https://anikela.com/discover-ten-cool-brands-from-alaras-hijabi-exhibition/' },
   ];
 
   return (
@@ -22,7 +24,7 @@ const Trending = () => {
               <h1 className="text-[60px] md:text-[75px] font-bold">{item.number}</h1>
 
               {/* Title */}
-              <h2 className="text-lg md:text-2xl font-semibold md:flex-1 text-center md:text-left">
+              <h2 className="text-lg md:text-2xl capitalize font-semibold md:flex-1 text-center md:text-left">
                 {item.title}
               </h2>
 
@@ -34,7 +36,7 @@ const Trending = () => {
               {/* Image */}
               <div className="w-20 h-20 md:w-24 md:h-24 mx-auto md:mx-0">
                 <img
-                  src={viral}
+                  src={item.image}
                   alt="Thumbnail"
                   className="object-cover w-full h-full"
                 />
