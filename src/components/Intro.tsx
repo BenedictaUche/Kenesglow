76a149973fec707df55e82a8392603e4197b983f
BenedictaUche <benedictaonyebuchi.uche@gmail.com>
2024-11-24 16:05:07 +0100

    update: changed intro texts

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,11 +4,11 @@ const Intro = () => {
       <p className="uppercase text-sm text-gray-500 tracking-widest">Hi, friends, I'm Kene</p>
 
       <div className="space-y-4">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">Happy Influencer</h1>
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">Charming Influencer</h1>
         <h1 className="text-2xl sm:text-3xl md:text-4xl italic">Fashion Lover</h1>
-        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">LAGOS NATIVE</h1>
-        <h1 className="text-2xl sm:text-3xl md:text-4xl italic">Dutiful Campaign Model</h1>
-        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">Devoted Youtuber</h1>
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">PROUD LAGOSIAN</h1>
+        <h1 className="text-2xl sm:text-3xl md:text-4xl italic">Graceful Campaign Model</h1>
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">Passionate Youtuber</h1>
       </div>
 
       <p className="text-base sm:text-lg md:text-xl text-gray-700 max-w-xl mx-auto leading-relaxed">
